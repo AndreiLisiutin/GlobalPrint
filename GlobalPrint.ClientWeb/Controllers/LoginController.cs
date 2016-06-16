47b087ea209852839f4a19d4e79de9bce6e015e3
Sergei Lisiutin <splisyutin@bars-open.ru>
2016-06-16 18:02:05 +0300

    Пробник странички логина

---
+++ GlobalPrint.ClientWeb/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using GlobalPrint.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlobalPrint.ClientWeb
+{
+    public class LoginController : BaseController
+    {
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
