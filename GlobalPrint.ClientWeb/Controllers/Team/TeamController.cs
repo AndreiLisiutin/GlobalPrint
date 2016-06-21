9487e043edace3ea112243dcf9b66dd77f6427ad
GOPbIHbI4_2D <lisutin.andrey@gmail.com>
2016-06-21 10:34:46 +0300

    Минутка юмора - страница "О нас"

---
+++ GlobalPrint.ClientWeb/Controllers/Team/TeamController.cs
@@ -0,0 +1,25 @@
+﻿using GlobalPrint.Server;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace GlobalPrint.ClientWeb
+{
+    public class TeamController : BaseController
+    {
+        // GET: Team/Team
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Team()
+        {
+            return View();
+        }
+    }
+}
