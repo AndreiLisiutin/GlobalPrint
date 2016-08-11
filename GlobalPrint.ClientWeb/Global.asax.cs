dbc762cb215d52830e8fa37e1324104388cae301
Andrei Lisiutin <lisutin.andrey@gmail.com>
2016-08-12 00:37:24 +0300

    Логирование ошибок в файл и на mail

--- GlobalPrint.ClientWeb/Global.asax.cs
+++ GlobalPrint.ClientWeb/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +10,28 @@ namespace GlobalPrint.ClientWeb
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected ILogUtility logUtility { get; set; }
+
+        public MvcApplication(/*ILogUtility logUtility*/) 
+            : base()
+        {
+            this.logUtility = new NlogUtility();
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             ModelBinders.Binders.Add(typeof(float?), new FloatModelBinder());
             ModelBinders.Binders.Add(typeof(float), new FloatModelBinder());
+            //this.logUtility.Info("Application Start");
+            //this.logUtility.Error("Error message");
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            this.logUtility.Error(exception, exception.Message);
         }
     }
 }
