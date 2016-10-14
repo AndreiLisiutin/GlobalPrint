4658142503475f87f08fa3a5b54821704a73dd5e
Sergei Lisiutin <93Grey10@mail.ru>
2016-10-15 00:20:57 +0300

    Улучшение лога об ошибке

---
+++ GlobalPrint.ClientWeb/Helpers/SystemConfigLayoutRenderer.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using NLog.LayoutRenderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace GlobalPrint.ClientWeb.Helpers
+{
+    [LayoutRenderer("system-configuration")]
+    public class SystemConfigurationLayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+#if DEBUG
+            builder.Append("DEBUG");
+#else
+            builder.Append("RELEASE"); 
+#endif
+
+        }
+    }
+}
