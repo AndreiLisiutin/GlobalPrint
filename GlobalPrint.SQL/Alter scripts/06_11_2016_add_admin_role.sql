24a1f70c199ec04eaacefa1fd5aecdb3528745be
Sergei Lisiutin <93Grey10@mail.ru>
2016-11-06 21:24:42 +0300

    Добавил в проект SQL Excel документ с выполнением скриптов на боевой.

---
+++ GlobalPrint.SQL/Alter scripts/06_11_2016_add_admin_role.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.role(role_id, name) VALUES (1, 'Admin');
---
+++ GlobalPrint.ServerBusinessLogic/Models/Domain/Users/UserRolesEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalPrint.ServerBusinessLogic.Models.Domain.Users
+{
+    public static class UserRolesEnum
+    {
+        /// <summary>
+        /// Administrator user role.
+        /// </summary>
+        public static readonly string Admin = "Admin";
+    }
+}
